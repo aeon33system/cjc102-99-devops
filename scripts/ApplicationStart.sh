c9a2e66ab515feae373bfc8795342ab72ea6ebb4
aeon33system <aeon33system@gmail.com>
2025-12-26 02:38:15 +0000

    ver5.0 first commit

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -p 5000:5000 --name flask-devops -d 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/iii-devops-ecr:latest
+docker run -p 80:5000 --name flask-web -d 805297316371.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-99-ecr-repo:latest
+
+sleep 20s
